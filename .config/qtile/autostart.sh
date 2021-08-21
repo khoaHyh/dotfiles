bedc8b183286b608352aab2a23e564dce4a34829
khoaHyh <khoahuynh.toronto@gmail.com>
2021-08-21 12:49:25 -0400

    edit i3 lock script

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # make sure time >= 5 if it isn't
-xautolock -detectsleep -time 5 -locker "i3lock -n -i ~/Wallpapers/groot-i-am-root-qhd-1920x1080.png" -killtime 10 -killer "systemctl suspend" &
+xautolock -detectsleep -time 5 -locker "i3lock -n -t -i ~/Wallpapers/groot-i-am-root-qhd-1920x1080.png" -killtime 10 -killer "systemctl suspend" &
 
 # start picom
 picom --experimental-backends &
