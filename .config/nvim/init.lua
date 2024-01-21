a4a5ceea534dcb6137df4240a524e9746fdc5931
khoaHyh <khoahuynh.toronto@gmail.com>
2024-01-21 15:44:26 -0500

    fix(nvim): Reset nvim config from scratch.
    - sane defaults
    - minimal plugins
    - actually understand what's going on lol

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,51 +1,76 @@
-require("user.keymaps")
-require("user.packages")
-require("user.options")
-require("user.vimwiki")
-require("user.cmp")
-require("user.lsp")
-require("user.telescope")
-require("user.treesitter")
-require("user.autopairs")
-require("user.comment")
-require("user.gitsigns")
-require("user.bufferline")
-require("user.lualine")
-require("user.toggleterm")
-require("user.project")
-require("user.impatient")
-require("user.whichkey")
-require("user.dashboard")
+local options = {
+	backup = false,
+	termguicolors = true,
+	cmdheight = 2,
+	expandtab = true,
+	shiftwidth = 4,
+	softtabstop = 4,
+	tabstop = 4,
+	autoindent = true,
+	number = true,
+	clipboard = "unnamedplus",
+	conceallevel = 0,
+	fileencoding = "utf-8",
+	hlsearch = true,
+	ignorecase = true,
+	mouse = "a",
+	showtabline = 2,
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	updatetime = 300,
+	writebackup = false,
+	cursorline = true,
+	relativenumber = false,
+	numberwidth = 4,
+	signcolumn = "yes",
+	wrap = false,
+	scrolloff = 8,
+}
 
-require("nvim-tree").setup({
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_tab = false,
-    hijack_cursor = false,
-    update_cwd = true,
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        },
-    },
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 500,
-    },
-    view = {
-        width = 30,
-        side = "left",
-        number = false,
-        relativenumber = false,
-    }
+-- set options via vimscript
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+		
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  "folke/neodev.nvim",
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "tsx", "go" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  },
+"sainnhe/gruvbox-material", 
 })
+
+-- set colorscheme
+vim.cmd([[colorscheme gruvbox-material]])
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_enable_bold = "1"
+
---
+++ .config/old-nvim/init.lua
@@ -0,0 +1,51 @@
+require("user.keymaps")
+require("user.packages")
+require("user.options")
+require("user.vimwiki")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.gitsigns")
+require("user.bufferline")
+require("user.lualine")
+require("user.toggleterm")
+require("user.project")
+require("user.impatient")
+require("user.whichkey")
+require("user.dashboard")
+
+require("nvim-tree").setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = true,
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+    },
+    view = {
+        width = 30,
+        side = "left",
+        number = false,
+        relativenumber = false,
+    }
+})
