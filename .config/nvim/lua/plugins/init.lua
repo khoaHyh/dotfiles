a3b7a8a6d06474116bf65d62f2821cfe3e48dd60
khoaHyh <khoahuynh.toronto@gmail.com>
2024-01-21 19:10:47 -0500

    find stuff fast af boi

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -10,5 +10,6 @@ return {
     "nvim-tree/nvim-web-devicons",
 	"nvim-telescope/telescope-media-files.nvim",
 	-- Comments
-	"JoosepAlviste/nvim-ts-context-commentstring"
+	"JoosepAlviste/nvim-ts-context-commentstring",
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
 	"nvim-telescope/telescope.nvim",
+    dependencies = {
+        {'nvim-lua/plenary.nvim'},
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    },
     config = function ()
         local status_ok, telescope = pcall(require, "telescope")
         if not status_ok then
@@ -87,7 +91,15 @@ return {
                     filetypes = { "png", "webp", "jpg", "jpeg" },
                     find_cmd = "rg", -- find command (defaults to `fd`)
                 },
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case"
+                }
             },
         })
+         -- This will load fzy_native and have it override the default file sorter
+        telescope.load_extension('fzf')
     end
 }
