bb310c6d1b5a3977c6ad9899e9e56eb60cfa0327
khoaHyh <khoahuynh.toronto@gmail.com>
2024-04-02 00:59:16 -0400

    buffer def and autocomplete

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,21 +6,20 @@ return {
 	},
 
 	config = function()
-        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local default_setup = function(server)
-          require('lspconfig')[server].setup({
-            capabilities = lsp_capabilities,
-          })
-        end
+		local default_setup = function(server)
+			require("lspconfig")[server].setup({
+				capabilities = lsp_capabilities,
+			})
+		end
 
 		local lspconfig = require("lspconfig")
 		local mason = require("mason")
 		require("mason-lspconfig").setup({
 			ensure_installed = { "clangd", "gopls", "tsserver", "eslint", "lua_ls", "marksman" },
-            handlers = { default_setup }
+			handlers = { default_setup },
 		})
-
 
 		mason.setup()
 
@@ -31,19 +30,46 @@ return {
 		lspconfig.lua_ls.setup({})
 		lspconfig.marksman.setup({})
 
-        local cmp = require("cmp")
-        cmp.setup({
-          sources = {
-            {name = 'nvim_lsp'},
-          },
-          mapping = cmp.mapping.preset.insert({
-            -- Enter key confirms completion item
-            ['<CR>'] = cmp.mapping.confirm({select = false}),
+		local cmp = require("cmp")
+		cmp.setup({
+			sources = {
+				{ name = "nvim_lsp" },
+			},
+			mapping = cmp.mapping.preset.insert({
+				-- Enter key confirms completion item
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				-- Previous item
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+				-- Next item
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+				-- Ctrl + space triggers completion menu
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+		})
 
-            -- Ctrl + space triggers completion menu
-            ['<C-Space>'] = cmp.mapping.complete(),
-          }),
-        })
+		-- Use LspAttach autocommand to only map the following keys
+		-- after the language server attaches to the current buffer
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				-- Enable completion triggered by <c-x><c-o>
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
+				-- Buffer local mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local opts = { buffer = ev.buf }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				--[[ vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) ]]
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<space>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+			end,
+		})
 	end,
 }
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,29 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", -- Highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate", -- Highlighting
 
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "tsx", "go" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  }
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"html",
+					"typescript",
+					"tsx",
+					"go",
+					"markdown",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
