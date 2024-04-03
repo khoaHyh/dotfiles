859afd73cab91fe66ac70c5006a4c89abaf51bc7
khoaHyh <khoahuynh.toronto@gmail.com>
2024-04-03 00:25:00 -0400

    remove conflicting keymap

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -66,9 +66,6 @@ return {
 				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
 				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "<space>f", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts)
 			end,
 		})
 	end,
